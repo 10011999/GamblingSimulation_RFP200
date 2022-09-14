5a24775a0b6f85bb917cfdca11c62e1c49d5e09f
10011999 <motewaraditya77@gmail.com>
2022-09-14 22:00:33 +0530

    Added Start Stake

---
+++ src/com/bridgelabz/GamblingSimulation.java
@@ -0,0 +1,10 @@
+package com.bridgelabz;
+
+public class GamblingSimulation {
+    final int StakePerGame = 1;
+
+    public static void main(String[] args) {
+        int perDayStake = 100;
+    }
+}
+
