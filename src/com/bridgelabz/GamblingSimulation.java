2dea583683a065ea64262660d6c3e3d0f127bf31
10011999 <motewaraditya77@gmail.com>
2022-09-14 22:09:01 +0530

    Added Either Win Or Loss

--- src/com/bridgelabz/GamblingSimulation.java
+++ src/com/bridgelabz/GamblingSimulation.java
@@ -2,9 +2,23 @@ package com.bridgelabz;
 
 public class GamblingSimulation {
     final int StakePerGame = 1;
-
+    static int totalStake=0;
     public static void main(String[] args) {
         int perDayStake = 100;
+        GamblingSimulation gamblingProblem = new GamblingSimulation();
+        gamblingProblem.isWinOrLoose(gamblingProblem.StakePerGame,perDayStake);
+    }
+    public void isWinOrLoose(int StakePerGame,int perDayStake){
+        int bet = (int)(Math.random() * 2);
+        if (bet == 1){
+            System.out.println("Win 1$");
+            totalStake = StakePerGame + perDayStake;
+        }
+        else {
+            System.out.println("Loose 1$");
+            totalStake = perDayStake - StakePerGame;
+        }
+        System.out.println("Total Stake is "+totalStake);
     }
 }
 
